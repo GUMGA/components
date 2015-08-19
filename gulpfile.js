1ac978f5056f3eb0f901c1cc72e79d333b4c9210
Guilherme Siquinelli <guiseek@gmail.com>
2015-08-19 14:50:42 -0300

    Reorganizada tasks do gulp, novas tasks compartilhadas via Google Docs com equipe

--- gulpfile.js
+++ gulpfile.js
@@ -14,94 +14,112 @@ var gulp = require('gulp')
 ,		ngAnnotate = require('gulp-ng-annotate')
 ,		sonar = require('gulp-sonar')
 ,		paths = {
-	src: ['./src/**/*.js','!**/*Spec.js'],
-	stylus: ['./src/index.styl'],
-	dist: ['./dist/']
-};
-
-gulp.task('hint',function(){
-	return gulp.src(paths.src)
-	.pipe(hint())
-	.pipe(hint.reporter(stylish))
-	})
-
-
-gulp.task('build-css',function(){
-	return gulp.src(paths.stylus)
-	.pipe(stylus())
-	.pipe(rename('gumga.min.css'))
-	.pipe(minifyCss())
-	.pipe(gulp.dest('./dist'))
-	})
-
-gulp.task('sonar', function () {
-    var options = {
-        sonar: {
-            host: {
-                url: 'http://192.168.25.201:9000'
-            },
-            jdbc: {
-                url: 'jdbc:mysql://192.168.25.201:3306/sonar',
-                username: 'gumga',
-                password: 'senha'
-            },
-            projectKey: 'sonar:components:1.1.0',
-            projectName: 'Components',
-            projectVersion: '1.1.0',
-            sources: 'src/',
-            language: 'js',
-            sourceEncoding: 'UTF-8',
-            javascript: {
-                lcov: {
-                    reportPath: 'test/sonar_report/lcov.info'
-                }
-            }
-        }
+    	src: ['./src/**/*.js', '!**/*Spec.js'],
+    	stylus: ['./src/index.styl'],
+    	dist: ['./dist/']
     };
 
+/**
+ * Minifica o CSS escrito com stylus, renomeia e coloca no dir. dist.
+ */
+gulp.task('minify-css',function(){
+	return gulp.src(paths.stylus)
+  	.pipe(stylus())
+  	.pipe(rename('gumga.min.css'))
+  	.pipe(minifyCss())
+  	.pipe(gulp.dest('./dist'))
+})
 
-  return gulp.src(paths.src,{ read: false })
-      .pipe(sonar(options))
-});
-
-gulp.task('build-dist',function(){
+/**
+ * Minifica o JS, renomeia e coloca no dir. dist.
+ */
+gulp.task('minify-js',function(){
 	return gulp.src(paths.src)
-	.pipe(ngAnnotate({remove: true,add:true}))
-	.pipe(sourcemaps.init())
-	.pipe(concat('gumga.min.js'))
-	.pipe(sourcemaps.write())
-	.pipe(uglify())
-	.pipe(gulp.dest('dist/'));
-	})
+  	.pipe(ngAnnotate({remove: true,add:true}))
+  	.pipe(sourcemaps.init())
+  	.pipe(concat('gumga.min.js'))
+  	.pipe(sourcemaps.write())
+  	.pipe(uglify())
+  	.pipe(gulp.dest('dist/'));
+})
 
+/**
+ * Executa o JSHint em cima dos arquivos Javascript.
+ */
+gulp.task('hint',function(){
+	return gulp.src(paths.src)
+  	.pipe(hint())
+  	.pipe(hint.reporter(stylish))
+})
 
-gulp.task('karma-tdd',function(done){
+/**
+ * Executa os testes unitários.
+ */
+gulp.task('tests',function(done){
 	var config = {
 		configFile: __dirname + '/karma.conf.js',
 		singleRun: isTravis
 	};
 	var server = new Server(config,done);
 	server.start();
-	})
+})
 
+/**
+ * Reporta a cobertura de testes.
+ */
+gulp.task('sonar', function () {
+  var options = {
+    sonar: {
+      host: {
+        url: 'http://192.168.25.201:9000'
+      },
+      jdbc: {
+        url: 'jdbc:mysql://192.168.25.201:3306/sonar',
+        username: 'gumga',
+        password: 'senha'
+      },
+      projectKey: 'sonar:components:1.1.0',
+      projectName: 'Components',
+      projectVersion: '1.1.0',
+      sources: 'src/',
+      language: 'js',
+      sourceEncoding: 'UTF-8',
+      javascript: {
+        lcov: {
+          reportPath: 'test/sonar_report/lcov.info'
+        }
+      }
+    }
+  };
+
+  return gulp.src(paths.src, { read: false })
+    .pipe(sonar(options))
+});
+
+/**
+ * Reporta complexidade do código.
+ */
 gulp.task('plato', function () {
 	return gulp.src(paths.src)
-	.pipe(plato('report', {
-		jshint: {
-			options: {
-				strict: true
-			}
-			},
-			complexity: {
-				trycatch: true
-			}
-			}));
-	});
+  	.pipe(plato('report', {
+  		jshint: {
+  			options: {
+  				strict: true
+  			}
+  		},
+  		complexity: {
+  			trycatch: true
+  		}
+  	}));
+});
 
-gulp.task('ngdocs',function(){
+/**
+ * Gerador de documentação.
+ */
+gulp.task('docs',function(){
 	var options = {
 		scripts: [
-		'./dist/gumga.min.js'
+		  './dist/gumga.min.js'
 		],
 		html5Mode: false,
 		startPage: '/directives',
@@ -111,21 +129,45 @@ gulp.task('ngdocs',function(){
 		directives: {
 			title: 'Directives',
 			glob: ['./src/directives/**/*.js','!**/*Spec.js']
-			},
-			services: {
-				title: 'Services',
-				glob: ['./src/services/**/*.js','!**/*Spec.js']
-			}
-			}))
-	.pipe(ngDocs.process(options))
-	.pipe(gulp.dest('./docs'))
-	})
+		},
+		services: {
+			title: 'Services',
+			glob: ['./src/services/**/*.js','!**/*Spec.js']
+		}
+	}))
+  	.pipe(ngDocs.process(options))
+  	.pipe(gulp.dest('./docs'))
+});
 
-gulp.task('ci',['karma-tdd','ngdocs']);
-gulp.task('dev',['hint','karma-tdd']);
-gulp.task('prod',['plato','build-dist','ngdocs','build-css'],function(){
-	gulp.watch(paths.src,['build-dist','build-css']);
-	});
-gulp.task('generate-doc',function(){
-	gulp.watch(paths.src,['build-dist','ngdocs'])
-})
+/**
+ * Usado para desenvolvimento,
+ * reexecuta as minificações CSS e JS a cada alteração.
+ */
+gulp.task('dev',function(){
+	gulp.watch(paths.src,['minify-js']);
+	gulp.watch(paths.stylus,['minify-css']);
+});
+
+/**
+ * Usado para desenvolvimento,
+ * reexecuta a minificação JS e os testes a cada alteração.
+ */
+gulp.task('tdd',function(){
+	gulp.watch(paths.src,['minify-js','tests']);
+});
+
+/**
+ * Reporta cobertura de testes e complexidade do código.
+ */
+gulp.task('report',['sonar','plato']);
+
+/**
+ * Valida qualidade do código com JSHint, executa os testes
+ * unitários e faz o report de cobertura e complexidade.
+ */
+gulp.task('ci-validate',['hint','tests','report']);
+
+/**
+ * Executa as validações, minifica o CSS, JS e gera documentação.
+ */
+gulp.task('ci-build',['ci-validate','minify-css','minify-js','docs']);
