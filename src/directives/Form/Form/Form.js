1828a1951cafc3eb61ed1fd1e6b5b94b37745d15
igorsantana <igorpsant95@gmail.com>
2015-10-29 16:46:25 -0200

    tentativa numero 4001 de não quebrar no travis

--- src/directives/Form/Form/Form.js
+++ src/directives/Form/Form/Form.js
@@ -53,7 +53,8 @@
 					if(inputIsValid !== true && inputIsValid !== false) throw 'É necessário passar um booleano como terceiro parâmetro [changeStateOfInput(inputName, validationType, inputIsValid, value)]';
 					let custom = ctrl.customMessage[inputName] ? ctrl.customMessage[inputName] : {};
 					let auxString = (custom[validationType] ? custom[validationType] : defaultMessages[validationType]);
-					let message = auxString.replace('{0}', inputName).replace('{1}', validationType.includes('range') ? ('mínimo de ' + value[0] + ' e máximo de ' + value[1]) : value);
+					let message = auxString.replace('{0}', inputName);
+					message = message.replace('{1}', validationType.includes('range') ? ('mínimo de ' + value[0] + ' e máximo de ' + value[1]) : value);
 					let objectSentToGumgaError = {validationType, message};
 					if(inputIsValid){
 						delete objectSentToGumgaError.message;
