e5bbc457ee9db1420ec12bdadfb333953db3c449
igorsantana <igorpsant95@gmail.com>
2015-10-30 10:17:43 -0200

    Tentativa número muito alto de fazer o travis funcionar

--- src/directives/Form/Form/Form.js
+++ src/directives/Form/Form/Form.js
@@ -48,17 +48,16 @@
 				}
 
 				function changeStateOfInput(inputName, validationType, inputIsValid, value){
-					if(!inputName) throw 'É necessário passar um valor válido como primeiro parâmetro [changeStateOfInput(inputName, validationType, inputIsValid, value)]';
-					if(!validationType) throw 'É necessário passar um valor válido como segundo parâmetro [changeStateOfInput(inputName, validationType, inputIsValid, value)]';
-					if(inputIsValid !== true && inputIsValid !== false) throw 'É necessário passar um booleano como terceiro parâmetro [changeStateOfInput(inputName, validationType, inputIsValid, value)]';
+					// if(!inputName) throw 'É necessário passar um valor válido como primeiro parâmetro [changeStateOfInput(inputName, validationType, inputIsValid, value)]';
+					// if(!validationType) throw 'É necessário passar um valor válido como segundo parâmetro [changeStateOfInput(inputName, validationType, inputIsValid, value)]';
+					// if(inputIsValid !== true && inputIsValid !== false) throw 'É necessário passar um booleano como terceiro parâmetro [changeStateOfInput(inputName, validationType, inputIsValid, value)]';
 					let custom 									= ctrl.customMessage[inputName] ? ctrl.customMessage[inputName] : {},
 							auxString 							= (custom[validationType] ? custom[validationType] : defaultMessages[validationType]),
 							message 								= auxString.replace('{0}', inputName).replace('{1}', validationType.includes('range') ? ('mínimo de ' + value[0] + ' e máximo de ' + value[1]) : value),
 							objectSentToGumgaError;
 
 					objectSentToGumgaError = (!inputIsValid) ? {message, validationType} : {validationType}
-
-					// ctrl.updateFormErrors(inputName, validationType, inputIsValid, message);
+					this.updateFormErrors(inputName, validationType, inputIsValid, message);
 					$scope.$broadcast('form-changed');
 					$scope.$broadcast(`${inputName.toLowerCase()}-${inputIsValid ? '' : 'in'}valid`, objectSentToGumgaError);
 					return this;
