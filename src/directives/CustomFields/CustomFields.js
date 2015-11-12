c7f0b4d5209f5baf1365816c733a6476cf1debef
Gui Seek <guiseek@gmail.com>
2015-11-12 09:43:53 -0200

    Custom Fields

--- src/directives/CustomFields/CustomFields.js
+++ src/directives/CustomFields/CustomFields.js
@@ -1,42 +1,55 @@
 (function(){
   'use strict';
 
-  CustomFields.$inject = ['$compile'];
-  function CustomFields($compile){
+  CustomFields.$inject = [];
+  function CustomFields(){
     return{
       restrict: 'E',
       scope: {
         fields: '='
       },
-      transclude: true,
       bindToController: true,
-      controller: ['$scope','$element','$attrs', function($scope, $element, $attrs) {
+      controller: ['$scope','$element','$attrs', '$http', '$compile', function($scope, $element, $attrs, $http, $compile) {
         let ctrl = this;
         if (!ctrl.fields) throw 'O componente gumgaCustomFields requer o escopo populado com os fields para geração do template.';
-        let template = [
-          '<div class="row" ng-repeat="f in ctrl.fields.gumgaCustomFields">'
-        , ' <div class="col-md-12">'
-        , '   <label ng-bind="f.field.name" gumga-translate-tag="f.field.translateKey"></label>'
-        , '   <div ng-switch="f.field.type" class="form-group">'
-        , '     <div ng-switch-when="TEXT">'
-        , '       <input type="text" ng-model="f.textValue" class="form-control" />'
-        , '     </div>'
-        , '     <div ng-switch-when="NUMBER">'
-        , '       <input type="number" ng-model="f.numberValue" class="form-control" />'
-        , '     </div>'
-        , '     <div ng-switch-when="DATE">'
-        , '       <input type="text" ng-model="f.dateValue" class="form-control" datepicker-popup="fullDate" is-open="opened" ng-click="opened= !opened" />'
-        , '     </div>'
-        , '     <div ng-switch-when="LOGIC">'
-        , '       <button type="button" class="btn btn-primary" ng-model="cf.logicValue" btn-checkbox btn-checkbox-true="true" btn-checkbox-false="false">'
-        , '         {{(cf.logicValue) ? "On" : "Off" }}'
-        , '       </button>'
-        , '     </div>'
-        , '   </div>'
-        , ' </div>'
-        , '</div>'
-        ];
-        $element.append($compile(template.join('\n'))($scope));
+
+        angular.forEach(ctrl.fields.gumgaCustomFields, (v) => {
+          if (v.field.type == 'SELECTION' && typeof v.field.options == 'string' && v.field.options.length > 0) {
+            $http.get(v.field.options).then((response) => {
+              v.field.options = response.data;
+            }, (error) => {
+              console.error(error);
+            });
+          }
+        });
+
+        let template = `
+        <div class="row" ng-repeat="f in ctrl.fields.gumgaCustomFields">
+          <div class="col-md-12">
+           <label ng-bind="f.field.name" gumga-translate-tag="f.field.translateKey"></label>
+           <div ng-switch="f.field.type" class="form-group">
+             <div ng-switch-when="TEXT">
+               <input type="text" ng-model="f.textValue" class="form-control" />
+             </div>
+             <div ng-switch-when="NUMBER">
+               <input type="number" ng-model="f.numberValue" class="form-control" />
+             </div>
+             <div ng-switch-when="DATE">
+               <input type="text" ng-model="f.dateValue" class="form-control" datepicker-popup="fullDate" is-open="opened" ng-click="opened = !opened" />
+             </div>
+             <div ng-switch-when="SELECTION">
+               <select ng-options="opt[f.field.optionValueField] as opt[f.field.optionLabelField] for opt in f.field.options[f.field.optionsCollection]" ng-model="f.textValue" class="form-control"></select>
+             </div>
+             <div ng-switch-when="LOGIC">
+               <button type="button" class="btn btn-primary" ng-model="cf.logicValue" btn-checkbox btn-checkbox-true="true" btn-checkbox-false="false">
+                 {{(cf.logicValue) ? "On" : "Off" }}
+               </button>
+             </div>
+           </div>
+         </div>
+        </div>
+        `;
+        $element.append($compile(template)($scope));
       }],
       controllerAs: 'ctrl',
     }
