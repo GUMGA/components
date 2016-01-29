1fabff279e445769655673ebccfd55cf1a8a02d8
igorpsantana <igorpsant95@gmail.com>
2016-01-29 14:42:41 -0200

    fix

--- src/components/Query/query/Query.js
+++ src/components/Query/query/Query.js
@@ -4,6 +4,7 @@
   Search.$inject = []
 
   function Search(){
+
     let template = `
       <div class="input-group">
        <input type="text" class="form-control" ng-model="searchField" ng-keyup="ctrl.doSearch(searchField, $event)" />
@@ -14,7 +15,7 @@
          <ul uib-dropdown-menu role="menu" aria-labelledby="single-button" class="dropdown-menu-search">
             <li role="menuitem" ng-repeat="(key, $value) in ctrl.mapFields">
                <a class="no-padding-search-fields">
-                 <label ng-click="$event.stopPropagation();">
+                 <label ng-click="$event.stopPropagation()">
                    <input type="checkbox" ng-model="$value.checkbox" />
                    <span><b>{{$value.label}}</b></span>
                  </label>
@@ -22,7 +23,8 @@
             </li>
           </ul>
           <button class="btn btn-default" type="button">
-           <span class="glyphicon glyphicon-filter"><span>
+           <span class="glyphicon glyphicon-filter">
+           <span>
           </button>
            <button class="btn btn-primary" type="button" ng-click="ctrl.doSearch(searchField)">
             <span> {{::ctrl.searchText}} </span>
@@ -34,31 +36,38 @@
     controller.$inject = ['$scope', '$element', '$attrs', '$transclude']
 
     function controller($scope, $element, $attrs, $transclude){
-      let ctrl = this
-      const hasAttr             = string  => !!$attrs[string]
+      const hasAttr             = string  => (!!$attrs[string]),
+            FIELD_ERR           = 'É necessário um parâmetro field na tag search-field.[<search-field field="foo"></search-field>]',
+            SEARCH_ERR          = 'É necessário passar uma função para o atributo "search". [search="foo(field, param)"]'
 
+      let ctrl = this
 
       ctrl.mapFields      = {}
+
+      if(!hasAttr('search')) console.error(SEARCH_ERR)
 
       $transclude((transcludeElement) => {
         let alreadySelected = false;
 
         [].slice.call(transcludeElement).forEach(value => {
-          if(value.nodeName !== 'SEARCH-FIELD') return
+
+          if(!value || value.nodeName !== 'SEARCH-FIELD') return
 
           let element   = angular.element(value),
               field     = element.attr('field'),
               checkbox  = !!$scope.$eval(element.attr('select')),
-              label     = $scope.$parent.$eval(element.attr('label')) ? $scope.$parent.$eval(element.attr('label')) : element.attr('label')
+              preLabel  = $scope.$parent.$eval(element.attr('label')) 
+              label     = preLabel ? preLabel : element.attr('label')
 
-          if(!field) console.error('É necessário um parâmetro field na tag search-field.[<search-field field="foo"></search-field>]')
-          if(checkbox) alreadySelected = true
+          if(!field)    console.error(FIELD_ERR)
+          if(checkbox)  alreadySelected = true
 
           ctrl.mapFields[field] = { checkbox, label, field }
         })
+
         if(!alreadySelected){
           for(var first in ctrl.mapFields) break
-          ctrl.mapFields[first].checkbox = true
+          if(first) ctrl.mapFields[first].checkbox = true
         }
        })
 
@@ -69,19 +78,17 @@
 
       function doSearch(param, event = { keyCode: 13 }){
         if(event.keyCode !== 13) return
-        let field =
-          Object
-            .keys(ctrl.mapFields)
-            .filter(value => !!ctrl.mapFields[value].checkbox)
-            .reduce((prev, next) => (prev += next.concat(',')), '')
-            .slice(0, -1)
+        let field = Object
+                    .keys(ctrl.mapFields)
+                    .filter(value => !!ctrl.mapFields[value].checkbox)
+                    .reduce((prev, next) => (prev += next.concat(',')), '')
+                    .slice(0, -1)
 
         if(field.length === 0) return
-
         ctrl.search({ field, param })
       }
 
-      if(!hasAttr('search')) console.error('É necessário passar uma função para o atributo "search". [search="foo(field, param)"]')
+      
     }
 
     return {
